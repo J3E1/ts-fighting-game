885cb774faf46294257d9a72db7f8765db33d513
Jeel <baimsnik@gmail.com>
2024-11-07 00:09:15 +0530

    Added vite config

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	base: '/ts-fighting-game/', // Replace with your repo name
+	build: {
+		outDir: 'dist', // Ensure it matches your GitHub Pages setup
+	},
+});
